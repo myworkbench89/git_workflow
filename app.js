5525454d790765b2d07f36867cc6dbe708a72a9e
myworkbench89 <myphpworkbench@gmail.com>
2025-09-21 17:08:14 -0400

    second commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
